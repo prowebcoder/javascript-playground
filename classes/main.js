59a898636ecd67a7ca2b405f8c17d8e3f93c449c
Prowebcoder <rahul@prowebcoder.com>
2024-01-01 10:46:44 +0530

    Classes Inheritance

--- classes/main.js
+++ classes/main.js
@@ -3,25 +3,34 @@ class Animal {
   constructor(name, sound) {
     this.name = name;
     this.sound = sound;
+    this.isSpeaking = false;
   }
 
   makeSound() {
     return this.sound;
   }
 
+  speak() {
+    this.isSpeaking = true;
+    return `${this.name} says ${this.makeSound()}`;
+  }
+
+  stopSpeaking() {
+    this.isSpeaking = false;
+    return `${this.name} stopped speaking`;
+  }
+
   displayInfo() {
-    return `<p>${this.name} says ${this.makeSound()}</p>`;
+    return `<p>${this.name} is a ${this.constructor.name.toLowerCase()}.</p>`;
   }
 }
 
 // Child class
 class Dog extends Animal {
   constructor(name) {
-    // Call the constructor of the parent class
     super(name, "Woof");
   }
 
-  // Override the makeSound method
   makeSound() {
     return `${super.makeSound()} Woof!`;
   }
@@ -30,20 +39,59 @@ class Dog extends Animal {
 // Child class
 class Cat extends Animal {
   constructor(name) {
-    // Call the constructor of the parent class
     super(name, "Meow");
   }
 
-  // Override the makeSound method
   makeSound() {
     return `${super.makeSound()} Meow!`;
+  }
+}
+
+// Additional child class
+class Bird extends Animal {
+  constructor(name) {
+    super(name, "Tweet");
+  }
+
+  makeSound() {
+    return `${super.makeSound()} Tweet!`;
+  }
+
+  fly() {
+    return `${this.name} is flying`;
+  }
+
+  displayInfo() {
+    return super.displayInfo() + `<p>${this.name} is a bird.</p>`;
   }
 }
 
 // Create instances of the classes
 const dog = new Dog("Buddy");
 const cat = new Cat("Whiskers");
+const bird = new Bird("Polly");
 
 // Display information on the webpage
-document.getElementById("output").innerHTML =
-  dog.displayInfo() + cat.displayInfo();
+function displayAnimalInfo() {
+  document.getElementById("output").innerHTML =
+    dog.displayInfo() + cat.displayInfo() + bird.displayInfo();
+}
+
+// Make animals speak when the button is clicked
+function makeAnimalsSpeak() {
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = "";
+
+  outputDiv.innerHTML += `<p>${dog.speak()}</p>`;
+  outputDiv.innerHTML += `<p>${cat.speak()}</p>`;
+  outputDiv.innerHTML += `<p>${bird.speak()}</p>`;
+
+  setTimeout(() => {
+    outputDiv.innerHTML += `<p>${dog.stopSpeaking()}</p>`;
+    outputDiv.innerHTML += `<p>${cat.stopSpeaking()}</p>`;
+    outputDiv.innerHTML += `<p>${bird.stopSpeaking()}</p>`;
+  }, 3000);
+}
+
+// Display initial information
+displayAnimalInfo();
